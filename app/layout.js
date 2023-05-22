b23a360bb3f8781866f91aff366cbe36deb8831e
Sachin Sharma <sachinkrsharmaa@gmail.com>
2023-05-22 16:14:35 +0530

    inital commit

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'Work — SachinsBlog',
+  description: 'Work with me',
+}
+ 
+export default function RootLayout({ children }) {
+ return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
